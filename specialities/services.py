e1c3a6b1aec831887e5a5861598f872220027def
Виктория Стаценко <statzenko.victorija@yandex.ru>
2024-04-25 20:12:24 +0300

    task-8

--- specialities/services.py
+++ specialities/services.py
@@ -1,36 +1,24 @@
 import models
 
 
-def get_speciality(spec_id: int):
-    speciality = models.Speciality.objects.get(id=spec_id)
+def get_speciality(target_id):
+    speciality = models.Speciality.objects.get(id=target_id)
     return speciality
 
 
-def add_speciality(spec_id: int, name: str, code: str, level: str, form: str):
-    if name is not None and code is not None and level is not None and form is not None:
-        speciality = models.Speciality.objects.create(name=name, code=code, level=level, form=form)
-        speciality.save()
-        spec_id = speciality.id
-    return spec_id
+def add_speciality(name: str, code: str, level: str, form: str):
+    speciality = models.Speciality.objects.create(name=name, code=code, level=level, form=form)
+    speciality.save()
+    target_id = speciality.id
+    return target_id
 
 
-def delite_speciality(spec_id: int):
-    speciality = models.Speciality.objects.get(id=spec_id)
+def delete_speciality(target_id: int):
+    speciality = models.Speciality.objects.get(id=target_id)
     speciality.delete()
     return speciality is None
 
 
-def update_speciality(spec_id: int, name: str = None, code: str = None, level: str = None, form: str = None):
-    speciality = models.Speciality.objects.get(id=spec_id)
-    if name is not None:
-        speciality.name = models.Speciality.objects.update(name=name)
-        speciality.save()
-    if code is not None:
-        speciality.code = models.Speciality.objects.update(code=code)
-        speciality.save()
-    if level is not None:
-        speciality.level = models.Speciality.objects.update(level=level)
-        speciality.save()
-    if form is not None:
-        speciality.form = models.Speciality.objects.update(form=form)
-        speciality.save()
+def update_speciality(name: str, code: str, level: str, form: str):
+    speciality = models.Speciality.objects.update(name=name, code=code, level=level, form=form)
+    speciality.save()
--- specialities/views.py
+++ specialities/views.py
@@ -1,4 +1,34 @@
 from django.http import JsonResponse, HttpRequest
 from universities.models import *
-
+import json
+from services import *
+from http import HTTPStatus
 # Здесь пишем view слой для специальностей
+
+
+def get_speciality_handler(request: HttpRequest) -> JsonResponse:
+    target_id = request.GET.get('id')
+    if not target_id:
+        return JsonResponse('{"status":"ok"}', status=HTTPStatus.BAD_REQUEST)
+
+
+def add_speciality_handler(request: HttpRequest) -> JsonResponse:
+    json_string = request.body.decode()
+    json_data = json.loads(json_string)
+    if not json_data["name"] and not json_data["code"] and not json_data["level"] and not json_data["form"]:
+        add_speciality(json_data["name"], json_data["code"], json_data["level"], json_data["form"])
+    return JsonResponse('{"status":"ok"}', status=HTTPStatus.OK)
+
+
+def delete_speciality_handler(request: HttpRequest) -> JsonResponse:
+    target_id = request.GET.get('id')
+    if not target_id:
+        return JsonResponse('{"status":"ok"}', status=HTTPStatus.BAD_REQUEST)
+
+
+def update_speciality_handler(request: HttpRequest) -> JsonResponse:
+    json_string = request.body.decode()
+    json_data = json.loads(json_string)
+    if json_data["name"] or json_data["code"] or json_data["level"] or json_data["form"]:
+        update_speciality(json_data["name"], json_data["code"], json_data["level"], json_data["form"])
+    return JsonResponse('{"status":"ok"}', status=HTTPStatus.OK)
