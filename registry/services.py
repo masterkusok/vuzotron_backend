e9bd2912e60c948e7110fb75b23ea91e712cedbd
masterkusok <fedyasetrakov@gmail.com>
2024-05-26 18:40:28 +0300

    Parser out of memory fixed

--- registry/services.py
+++ registry/services.py
@@ -3,9 +3,14 @@ from http import HTTPStatus
 import requests
 import zipfile
 import shutil
+import os
+from xml.etree.ElementTree import *
 from universities.services import *
 import backend.settings
 from .xml_parser import *
+
+START_EVENT = 'start'
+END_EVENT = 'end'
 
 
 def pull_registry_data(url: str) -> bool:
@@ -37,22 +42,50 @@ def pull_registry_data(url: str) -> bool:
         registry_hash = hashlib.md5()
         while chunk := file.read():
             registry_hash.update(chunk)
+    try:
+        actual_hash = cacher.get('hash')
+        if actual_hash == registry_hash.digest():
+            print('registry has not changed')
+            return False
+        cacher.set('hash', registry_hash.digest())
+    except ConnectionError:
+        print('Cant access RedisCache')
 
-    actual_hash = cacher.get('hash')
-    if actual_hash == registry_hash.digest():
-        print('registry has not changed')
-        return False
-
-    cacher.set('hash', registry_hash.digest())
     with zipfile.ZipFile('registry.zip', 'r') as zip_origin:
         zip_origin.extractall('temp/')
 
-    ok, parsed_universities = parse_file('temp/' + os.listdir('temp/')[0])
-
-    if not ok:
-        print('cant handle parsing ')
+    try:
+        parse_file('temp/' + os.listdir('temp/')[0])
+    except Exception:
+        clear_temp_data()
         return False
 
+    clear_temp_data()
+    return True
+
+
+def clear_temp_data():
+    """
+    This method is used to clear temp folders with registry files
+    """
+    if os.path.exists('registry.zip'):
+        os.remove('registry.zip')
+    if os.path.exists('temp'):
+        shutil.rmtree('temp')
+
+
+def push_universities_to_db(parsed_universities: list[(University, list[Speciality])]):
+    """
+    This method is used to write new universities from registry to database
+    Parameters
+    ----------
+    parsed_universities : list[tuple(University, list[Speciality])]
+        list of new universities to be added
+
+    Returns
+    -------
+
+    """
     university_services = UniversityServices()
     print('Moving universities to DB')
     for university, specialities in parsed_universities:
@@ -76,6 +109,113 @@ def pull_registry_data(url: str) -> bool:
             spec.save()
         db_university.specialities.set(specialities)
 
-    os.remove('registry.zip')
-    shutil.rmtree('temp')
-    return True
+
+def parse_file(path: str):
+    """
+    This function is used to parse the xml file of Obrdnadzor registry
+    Parameters
+    ----------
+    path : str
+        path to registry.xml file
+    Returns
+    -------
+    (bool, list[(University, list[Speciality])])
+        Tuple, with bool, which indicates if registry was parsed successfully, and list on pairs of University and its specialities
+    """
+    university_list = []
+    if not os.path.exists(path):
+        # файл не найден
+        return
+
+    in_license = True
+    skip = False
+    spec_skip = False
+    address = ""
+    short_name = ""
+    full_name = ""
+    sys_guid = ""
+    spec_name = ""
+    spec_code = ""
+    level = ""
+
+    current_specs = []
+
+    for event, elem in iterparse(path, events=(START_EVENT, END_EVENT)):
+        # Читаем новый сертификат
+        if elem.tag == 'license' and event == START_EVENT:
+            if not skip:
+                print(f'University found, current number of universities is {len(university_list)}')
+                if len(university_list) >= 100:
+                    push_universities_to_db(university_list)
+                    university_list = []
+
+                university = University(short_name=short_name, full_name=full_name, city=address, region='no_data',
+                                        sys_guid=sys_guid)
+                university_list.append((university, current_specs))
+
+            skip = False
+            spec_skip = False
+            in_license = True
+
+        if skip:
+            elem.clear()
+            continue
+
+        if elem.tag == 'schoolTypeName' and event == START_EVENT:
+            if elem.text is None or elem.text != 'Образовательная организация высшего образования':
+                skip = True
+
+        if in_license and elem.tag == 'lawAddress' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                skip = True
+            address = elem.text
+
+        if in_license and elem.tag == 'schoolName' and event == START_EVENT:
+            if elem.text is None or len(elem.text) >= 254 or len(elem.text) == 0:
+                skip = True
+            full_name = elem.text
+
+        if in_license and elem.tag == 'shortName' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                skip = True
+            short_name = elem.text
+
+        if in_license and elem.tag == 'sysGuid' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                skip = True
+            sys_guid = elem.text
+
+        if elem.tag == 'supplements' and event == START_EVENT:
+            current_specs = []
+
+        if elem.tag == 'licensedProgram' and event == START_EVENT:
+            spec_skip = False
+            spec_name = ""
+            spec_code = ""
+            level = ""
+
+        if (not spec_skip) and elem.tag == 'licensedProgram' and event == END_EVENT:
+            spec = Speciality(name=spec_name, code=spec_code, level=level, form="no_data")
+            current_specs.append(spec)
+
+        if spec_skip:
+            elem.clear()
+            continue
+
+        if elem.tag == 'code' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                spec_skip = True
+            spec_code = elem.text
+
+        if elem.tag == 'name' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                spec_skip = True
+            spec_name = elem.text
+
+        if elem.tag == 'eduLevelName' and event == START_EVENT:
+            if elem.text is None or len(elem.text) == 0:
+                spec_skip = True
+            level = elem.text
+        elem.clear()
+    if len(university_list) != 0:
+        push_universities_to_db(university_list)
